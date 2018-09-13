e846b136dad3e0b9845bbf7bd2882881b6a4c5f7
David Gridley <davidgridley21@gmail.com>
2018-09-13 15:04:22 +0100

    added delete button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 const tweetSubmit = document.querySelector("form");
 let counterGlobal = 0;
 
+const updateDeleteButtons = () => {
+   const deleteButtons = document.querySelectorAll(".delete_button");
+   console.log(deleteButtons);
+   return deleteButtons;
+};
+
 tweetSubmit.addEventListener("submit", function(event){
     event.preventDefault();
-    const tweetContent = document.querySelector(".twitter_form").value;
+    const tweetContent = document.querySelector(".twitter_form");
+    const tweetText = tweetContent.value;
     const tweetToFeed = document.createElement("li");
-    tweetToFeed.textContent = tweetContent;
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete_button";
+    tweetToFeed.textContent = tweetText;
+    tweetToFeed.appendChild(deleteButton);
     const twitterFeed = document.querySelector(".twitter_feed");
-    twitterFeed.insertBefore(tweetToFeed, twitterFeed.firstChild);
-    const twitterBox = document.querySelector(".twitter_form");
-    twitterBox.value = "";
+    const charNum = document.querySelector(".counter").textContent;
+    const warning = document.querySelector(".warning");
+    if (charNum <= 280) { 
+        twitterFeed.insertBefore(tweetToFeed, twitterFeed.firstChild);
+        const twitterBox = document.querySelector(".twitter_form");
+        twitterBox.value = "";
+        warning.textContent = "";
+        updateDelete(deleteButton);
+    } else {
+        warning.textContent = "Over max character limit!";
+        warning.style.color = "red";
+    };
 });
 
 const characters = document.querySelector(".twitter_form");
@@ -19,3 +38,11 @@ characters.addEventListener("input", function(inputEvent){
    charNum.textContent = charCount;
    (charCount >= 281) ? charNum.style.color = "red" : charNum.style.color = "black";
 });
+
+function updateDelete(butt){
+    butt.addEventListener("click", function(event) {
+      const tweet = event.target.parentNode.parentNode;
+      const targ = event.target.parentNode;
+      tweet.removeChild(targ);  
+    });
+};
